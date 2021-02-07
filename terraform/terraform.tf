3a30e8ed2678c0b6940e1e5b72e4b0acaf3b8980
Jack Caldwell <jackcaldwell92@googlemail.com>
2021-02-07 12:04:13 +0000

    update task def and workflow

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -5,134 +5,76 @@ provider "aws" {
 }
 
 locals {
-  # The name of the ECR cluster to be created
-  aws_ecr_repository_name = var.aws_resource_prefix
-  # The name of the ECS cluster to be created
-  aws_ecs_cluster_name = "${var.aws_resource_prefix}-cluster"
-  # The name of the ECS service to be created
-  aws_ecs_service_name = "${var.aws_resource_prefix}-service"
-  # The name of the application load balancer to be created
-  aws_lb_name = "${var.aws_resource_prefix}-load-balancer"
-  # The base name for the target groups to be created
-  aws_target_group_name = "${var.aws_resource_prefix}-target-group"
+
 }
 
-resource "aws_ecr_repository" "app_repository" {
-  name = local.aws_ecr_repository_name
+resource "aws_ecs_cluster" "main" {
+  name = "${var.resource_prefix}-cluster"
 }
 
-resource "aws_ecs_task_definition" "task_definition" {
-  family                   = var.aws_resource_prefix
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-  container_definitions    = file("task-definitions/service.json")
-}
+# VPC Definition
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.70.0"
 
-resource "aws_ecs_cluster" "ecs_cluster" {
-  name = local.aws_ecs_cluster_name
-}
+  name = "${var.resource_prefix}-vpc"
+  cidr = "10.50.0.0/16"
 
-resource "aws_vpc" "main" {
-  cidr_block         = "10.0.0.0/16"
-  enable_dns_support = true
-}
+  azs             = ["eu-west-1a", "eu-west-1b"]
+  public_subnets  = ["10.50.11.0/24", "10.50.12.0/24"]
+  private_subnets = ["10.50.21.0/24", "10.50.22.0/24"]
 
-resource "aws_internet_gateway" "main" {
-  depends_on = [aws_vpc.main]
-  vpc_id     = aws_vpc.main.id
-}
+  single_nat_gateway = true
 
-resource "aws_subnet" "a" {
-  depends_on        = [aws_vpc.main]
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.0.0/24"
-  availability_zone = "${var.aws_region}a"
-}
+  enable_nat_gateway   = true
+  enable_vpn_gateway   = false
+  enable_dns_hostnames = true
 
-resource "aws_subnet" "b" {
-  depends_on        = [aws_vpc.main]
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "${var.aws_region}b"
-}
-
-resource "aws_security_group" "main" {
-  name       = "${var.aws_resource_prefix}-sg"
-  depends_on = [aws_vpc.main]
-  vpc_id     = aws_vpc.main.id
-}
-
-resource "aws_security_group_rule" "ingress" {
-  type              = "ingress"
-  protocol          = "-1"
-  to_port           = 0
-  from_port         = 0
-  security_group_id = aws_security_group.main.id
-  self              = false
-  cidr_blocks       = ["10.0.0.0/24", "10.0.1.0/24"]
-}
-
-resource "aws_security_group_rule" "egress" {
-  type              = "egress"
-  protocol          = "-1"
-  to_port           = 0
-  from_port         = 0
-  security_group_id = aws_security_group.main.id
-  self              = false
-  cidr_blocks       = ["10.0.0.0/24", "10.0.1.0/24"]
-}
-
-
-resource "aws_lb" "main" {
-  name               = local.aws_lb_name
-  internal           = false
-  load_balancer_type = "application"
-  subnets            = [aws_subnet.a.id, aws_subnet.b.id]
-  security_groups    = [aws_security_group.main.id]
-  depends_on         = [aws_subnet.a, aws_subnet.b, aws_security_group.main]
-}
-
-resource "aws_lb_target_group" "instance" {
-  name     = "${local.aws_target_group_name}-instance"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
-}
-
-resource "aws_lb_target_group" "ip" {
-  name        = "${local.aws_target_group_name}-ip"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = aws_vpc.main.id
-  target_type = "ip"
-}
-
-resource "aws_lb_listener" "main" {
-  load_balancer_arn = aws_lb.main.arn
-  port              = 80
-  protocol          = "HTTP"
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.ip.arn
+  tags = {
+    Terraform = "true"
   }
 }
 
-resource "aws_ecs_service" "ecs_service" {
-  name            = local.aws_ecs_service_name
-  task_definition = "${var.aws_resource_prefix}:${aws_ecs_task_definition.task_definition.revision}"
-  desired_count   = 1
-  launch_type     = "FARGATE"
-  depends_on      = [aws_lb.main, aws_lb_target_group.ip]
-  network_configuration {
-    subnets         = [aws_subnet.a.id, aws_subnet.b.id]
-    security_groups = [aws_security_group.main.id]
-  }
-  cluster = local.aws_ecs_cluster_name
-  load_balancer {
-    container_name   = "app"
-    container_port   = 80
-    target_group_arn = aws_lb_target_group.ip.arn
-  }
+
+module "zapp" {
+  source  = "aoggz/fargate-app/aws"
+  version = "2.0.0"
+
+  resource_prefix                           = var.resource_prefix
+  ecs_cluster_id                            = aws_ecs_cluster.cluster.id
+  acm_certificate_domain                    = var.acm_certificate_domain
+  log_retention_in_days                     = 30
+  app_domain                                = var.app_domain           # must be a subdomain of the acm_certificate_domain
+  route53_hosted_zone_id                    = var.hosted_zone_id       # Route 53 hosted zone id in which alias to load balancer
+  task_count                                = var.app_instance_count   # Number of instances to run
+  reverse_proxy_cpu                         = var.reverse_proxy_cpu    # Number of CPU Units for reverse_proxy container
+  reverse_proxy_memory                      = var.reverse_proxy_memory # MB of RAM for reverse_proxy container
+  reverse_proxy_version                     = "1.0.0"                  # Docker image tag of nginx_reverse_proxy container
+  reverse_proxy_cert_state                  = "Lancashire"
+  reverse_proxy_cert_locality               = "Preston"
+  reverse_proxy_cert_organization           = "Okount"
+  reverse_proxy_cert_organizational_unit    = "-"
+  reverse_proxy_cert_email_address          = "jack@okount.com"
+  xray_cpu                                  = var.xray_cpu    # Number of CPU Units for xray container
+  xray_memory                               = var.xray_memory # MB of RAM for xray container
+  alb_internal                              = true
+  alb_subnets_public                        = var.public_subnet_ids
+  alb_subnets_private                       = var.private_subnet_ids
+  alb_listener_default_action               = "redirect" # Note: if redirect is used, another lb_listener_rule must be created that forwards to the target group
+  alb_listener_default_redirect_host        = var.redirect_host
+  alb_listener_default_redirect_port        = "443"
+  alb_listener_default_redirect_protocol    = "HTTPS"
+  alb_listener_default_redirect_status_code = "HTTP_302"
+  vpc_id                                    = var.vpc_id
+  web_cpu                                   = var.web_cpu     # Number of CPU Units for web container
+  web_memory                                = var.web_memory  # MB of RAM for web container
+  web_image                                 = var.web_image   # Name of Docker image to use for web container
+  web_version                               = var.web_version # Version of Docker image to use for web container
+  web_environment_variables = [
+    {
+      name  = "ASPNETCORE_ENVIRONMENT",
+      value = var.aspnetcore_environment,
+    },
+  ]
 }
+
